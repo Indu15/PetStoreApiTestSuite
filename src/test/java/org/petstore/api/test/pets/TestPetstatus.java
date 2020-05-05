28f85f5107c4d8df37b4c1fd84c564d093cca852
Indumathi Duraisamy <[Dinduduraisamy15@gmail.com>
2020-05-05 07:05:20 -0400

    Completed PetUpload Method

--- src/test/java/org/petstore/api/test/pets/TestPetstatus.java
+++ src/test/java/org/petstore/api/test/pets/TestPetstatus.java
@@ -4,6 +4,8 @@ package org.petstore.api.test.pets;
 import org.junit.Test;
 import org.petstore.api.test.util.PetStoreApiTestConstants;
 
+import java.io.File;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
@@ -21,6 +23,19 @@ public class TestPetstatus implements PetStoreApiTestConstants {
                 .body("[0].category.name", notNullValue());
            //     .body("[0].status", equalTo("sold"));
 
+    }
+
+    @Test
+    public void testPetuploadimage() {
+        given()
+                .pathParam("petId","1143")
+                .contentType("multipart/form-data")
+                .multiPart(new File("C:\\download.jpg"))
+                .when()
+                .post(Base_URL+"/pet/{petId}/uploadImage")
+                .then()
+                .statusCode(200)
+                .body("type",notNullValue());
     }
 }
 
